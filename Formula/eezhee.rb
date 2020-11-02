5db82089235dbdd1a24585cee654c8cabbc01c96
Athir Nuaimi <anuaimi@users.noreply.github.com>
2020-11-02 15:00:55 -0500

    Update eezhee.rb
    forgot to update the url for v0.2.7 release

--- Formula/eezhee.rb
+++ Formula/eezhee.rb
@@ -1,7 +1,7 @@
 class Eezhee < Formula
   desc "Install Eezhee CLIs"
   homepage "http://github.com/eezhee/eezhee"
-  url "https://github.com/eezhee/eezhee/releases/download/v0.2.6/eezhee"
+  url "https://github.com/eezhee/eezhee/releases/download/v0.2.7/eezhee"
   sha256 "6cd45e4b98b91ecdfa9a61c057b156cbbc95b9626023108796f2ff958e9e451e"
   version "0.2.7"
   bottle :unneeded
